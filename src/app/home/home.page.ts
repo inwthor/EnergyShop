db7266958bf7972a8abd8df79c10aa4b7a1c4c09
inwthor <mini.ninja.assassin@gmail.com>
2020-11-17 02:03:03 +0700

    fist

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public barcodeScanner: BarcodeScanner,
+  ) {}
 
+  ngOnInit() {
+    
+  }
+  qr() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      alert(console.log('Barcode data', barcodeData));
+     }).catch(err => {
+        alert( console.log('Error', err));
+     });
+  }
 }
